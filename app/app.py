3d9b2571c6c8c6fef17668063dc5124be3d23e28
Nick Venenga <nick@venenga.com>
2022-06-05 21:56:02 -0400

    Add hostname to Python output

--- app/app.py
+++ app/app.py
@@ -1,3 +1,5 @@
+import socket
+
 from datetime import datetime
 from flask import Flask, Response, request
 
@@ -6,7 +8,8 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def _():
     headers = "\n".join([f"{k}={v}" for k, v in request.headers.items()])
+    hostname = socket.gethostname()
     return Response(
-        f"{datetime.now().isoformat()}\n\n{headers}",
+        f"{datetime.now().isoformat()}\n\n{headers}\n\n{hostname}",
         mimetype="text/plain",
     )
